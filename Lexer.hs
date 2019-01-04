d6d598e28b1a34ae9620c21a733a51e407454938
James Thomas <codeactor@protonmail.com>
2019-01-04 16:35:33 +0000

    initial commit

---
+++ Lexer.hs
@@ -0,0 +1,40 @@
+module Lexer (Token(..), tokenize) where
+
+
+import Data.Char
+
+
+data Token = TokOpenParen
+           | TokCloseParen
+           | TokEnd
+           deriving (Show, Eq)
+
+
+tokenize :: String -> [Token]
+tokenize [] = []
+tokenize (c:cs)
+    | c == '('      = TokOpenParen       : tokenize cs
+    | c == ')'      = TokCloseParen      : tokenize cs
+    | isSpace c     = tokenize cs
+    | otherwise     = error $ "Cannot tokenize " ++ [c]
+
+
+lookAhead :: [Token] -> Token
+lookAhead [] = TokEnd
+lookAhead (c:cs) = c
+
+
+accept :: [Token] -> [Token]
+accept [] = error "Nothing to accept"
+accept (t:ts) = ts
+
+
+--identifier :: Char -> String -> [Token]
+--identifier c cs = let (str, cs') = span isAlphaNum cs in
+--                  TokIdent (c:str) : tokenize cs'
+--
+--
+--number :: Char -> String -> [Token]
+--number c cs =
+--    let (digs, cs') = span isDigit cs in
+--    TokNum (read (c:digs)) : tokenize cs'
---
+++ Main.hs
@@ -0,0 +1,9 @@
+module Main (main) where
+
+
+import Lexer (tokenize)
+
+
+main :: IO()
+main = do
+        print $ tokenize "()"
