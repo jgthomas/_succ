d9f2daf157301b5b44e7d54dfaaaeefd2246afdc
James Thomas <codeactor@protonmail.com>
2019-01-06 13:13:56 +0000

    very basic generator added

--- Generator.hs
+++ Generator.hs
@@ -1,9 +1,20 @@
 
-module Generator (generate) where
+module Generator (generate, progString) where
 
 
 import Parser
 
 
-generate :: Tree -> String
-generate x = ".globl main\nmain:\nmovq $5, %rax\nret\n"
+generate :: Tree -> [String]
+
+generate (ProgramNode tree) = generate tree
+
+generate (FunctionNode name tree) = [name] ++ generate tree
+
+generate (StatementNode kwd tree) = generate tree
+
+generate (ExpressionNode n) = [(show n)]
+
+
+progString :: [String] -> String
+progString (f:e:str) = ".globl " ++ f ++ "\n" ++ f ++ ":\n" ++ "movq $" ++ e ++ ", %rax\nret\n"
--- Main.hs
+++ Main.hs
@@ -7,7 +7,7 @@ import System.Environment (getArgs)
 
 import Lexer (tokenize)
 import Parser (parse)
-import Generator (generate)
+import Generator
 
 
 main :: IO()
@@ -17,5 +17,8 @@ main = do
         contents <- hGetContents handle
         print $ tokenize contents
         print $ parse $ tokenize contents
-        writeFile "output.s" $ generate $ parse $ tokenize contents
+        print $ generate $ parse $ tokenize contents
+        let outfileText = progString $ generate $ parse $ tokenize contents
+        print outfileText
+        writeFile "output.s" outfileText
         hClose handle
