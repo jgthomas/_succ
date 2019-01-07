f48c2e2f128078b0ed4510649914911e887cf513
James Thomas <codeactor@protonmail.com>
2019-01-07 09:25:34 +0000

    assembly string now built directly during tree traverse

--- Generator.hs
+++ Generator.hs
@@ -1,30 +1,35 @@
 
-module Generator (extractFrom, progString) where
+module Generator (extractFrom) where
 
 
+import Lexer
 import Parser
 
 
-extractFrom :: Tree -> [String]
+extractFrom :: Tree -> String
 
 extractFrom (ProgramNode tree) = extractFrom tree
 
-extractFrom (FunctionNode name tree) = [name] ++ extractFrom tree
+extractFrom (FunctionNode name tree) = functionName name ++ extractFrom tree
 
-extractFrom (ReturnNode tree) = extractFrom tree
+extractFrom (ReturnNode tree) = extractFrom tree ++ returnStatement
 
-extractFrom (ConstantNode n) = [(show n)]
+extractFrom (ConstantNode n) = returnValue (show n)
 
-extractFrom (UnaryNode tree unop) = extractFrom tree ++ [(show unop)]
-
-
-progString :: [String] -> String
-progString (f:n:str) = (header f) ++ (returnVal n)
+extractFrom (UnaryNode tree unop) = extractFrom tree ++ unary unop
 
 
-header :: String -> String
-header f = ".globl " ++ f ++ "\n" ++ f ++ ":\n"
+functionName :: String -> String
+functionName f = ".globl " ++ f ++ "\n" ++ f ++ ":\n"
 
+returnValue :: String -> String
+returnValue n = "movq $" ++ n ++ ", %rax\n"
 
-returnVal :: String -> String
-returnVal n = "movq $" ++ n ++ ", %rax\nret\n"
+returnStatement :: String
+returnStatement = "ret\n"
+
+unary :: UnaryOperator -> String
+unary o
+   | o == Negation      = "neg %rax\n"
+   | o == BitwiseCompl  = "not %rax\n"
+   | o == LogicNegation = "cmpq $0, %rax\nmovq $0, %rax\nsete %al\n"
--- Main.hs
+++ Main.hs
@@ -27,15 +27,17 @@ main = do
         print infileName
         handle <- openFile infileName ReadMode
         contents <- hGetContents handle
+
         let tokens = tokenize contents
         print tokens
+
         let parsedTree = parse tokens
         print parsedTree
-        let extractedNodes = extractFrom parsedTree
-        print extractedNodes
-        let outfileText = progString extractedNodes
-        print outfileText
-        writeFile outfileName outfileText
+
+        let assembly = extractFrom parsedTree
+        print assembly
+
+        writeFile outfileName assembly
         system $ "gcc " ++ outfileName ++ " -o " ++ (dropExtension outfileName)
         system $ "rm " ++ outfileName
         hClose handle
