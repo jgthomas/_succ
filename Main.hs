b4c58c1c524ab24688f8b2fe8e8131489538b0f4
James Thomas <codeactor@protonmail.com>
2019-01-08 11:34:38 +0000

    add debug tokenize and parse prints

--- Main.hs
+++ Main.hs
@@ -28,10 +28,20 @@ main = do
         --print $ genASM $ parse $ tokenize "int main() {return 1 + 2;}"
         --print $ genASM $ parse $ tokenize "int main() {return 2 / 2;}"
 
+        --print $ parse $ tokenize "int main() {return 6 / 3 / 2;}"
+        --print $ parse $ tokenize "int main() {return 6 * 3 / 2;}"
+        --print $ parse $ tokenize "int main() {return 3 - 3 + 3;}"
+        --print $ parse $ tokenize "int main() {return 3 / 3 * 3;}"
+        --print $ parse $ tokenize "int main() {return 6 - 3 + 2;}"
+
         args <- getArgs
         let infileName = head args
         handle <- openFile infileName ReadMode
         contents <- hGetContents handle
+
+        -- debugging
+        print $ tokenize contents
+        print $ parse $ tokenize contents
 
         let outfileName = (dropExtension infileName) ++ ".s"
         let assembly = genASM $ parse $ tokenize contents
@@ -40,5 +50,5 @@ main = do
            writeFile outfileName assembly
 
         system $ "gcc " ++ outfileName ++ " -o " ++ (dropExtension outfileName)
-        system $ "rm " ++ outfileName
+        --system $ "rm " ++ outfileName
         hClose handle
