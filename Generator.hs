72dfebaf06a19e8141c134408d99d0c35e46020a
James Thomas <codeactor@protonmail.com>
2019-01-09 09:17:33 +0000

    rename function2

--- Generator.hs
+++ Generator.hs
@@ -14,7 +14,7 @@ genASM (FunctionNode name tree) = functionName name ++ genASM tree
 
 genASM (ReturnNode tree) = genASM tree ++ returnStatement
 
-genASM (ConstantNode n) = pushValue n
+genASM (ConstantNode n) = loadValue n
 
 genASM (UnaryNode tree op) = genASM tree ++ unary op
 
@@ -24,14 +24,8 @@ genASM (BinaryNode left right op) = binary (genASM left) (genASM right) op
 functionName :: String -> String
 functionName f = ".globl " ++ f ++ "\n" ++ f ++ ":\n"
 
-pushValue :: Int -> String
-pushValue n = "movq $" ++ (show n) ++ ", %rax\n"
-
-popResult :: String
-popResult = "popq %rax\n"
-
-storeResult :: String
-storeResult = "pushq %rax\n"
+loadValue :: Int -> String
+loadValue n = "movq $" ++ (show n) ++ ", %rax\n"
 
 returnStatement :: String
 returnStatement = "ret\n"
@@ -43,8 +37,8 @@ unary o
    | o == LogicNegation = "cmpq $0, %rax\nmovq $0, %rax\nsete %al\n"
 
 binary :: String -> String -> Operator -> String
-binary push1 push2 o
-   | o == Plus       = push1 ++ "pushq %rax\n" ++ push2 ++ "popq %rcx\n" ++ "addq %rcx, %rax\n"
-   | o == Multiply   = push1 ++ "pushq %rax\n" ++ push2 ++ "popq %rcx\n" ++ "imul %rcx, %rax\n"
-   | o == Minus      = push2 ++ "pushq %rax\n" ++ push1 ++ "popq %rcx\n" ++ "subq %rcx, %rax\n"
-   | o == Divide     = push2 ++ "movq %rax, %rbx\n" ++ push1 ++ "cqto\n" ++ "idivq %rbx\n"
+binary val1 val2 o
+   | o == Plus       = val1 ++ "pushq %rax\n" ++ val2 ++ "popq %rcx\n" ++ "addq %rcx, %rax\n"
+   | o == Multiply   = val1 ++ "pushq %rax\n" ++ val2 ++ "popq %rcx\n" ++ "imul %rcx, %rax\n"
+   | o == Minus      = val2 ++ "pushq %rax\n" ++ val1 ++ "popq %rcx\n" ++ "subq %rcx, %rax\n"
+   | o == Divide     = val2 ++ "movq %rax, %rbx\n" ++ val1 ++ "cqto\n" ++ "idivq %rbx\n"
