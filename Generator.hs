d772ebdbf586b04befeee0631824bd6abae17e12
James Thomas <codeactor@protonmail.com>
2019-01-10 10:33:40 +0000

    add spacing

--- Generator.hs
+++ Generator.hs
@@ -24,17 +24,21 @@ genASM (BinaryNode left right op) = binary (genASM left) (genASM right) op
 functionName :: String -> String
 functionName f = ".globl " ++ f ++ "\n" ++ f ++ ":\n"
 
+
 loadValue :: Int -> String
 loadValue n = "movq $" ++ (show n) ++ ", %rax\n"
 
+
 returnStatement :: String
 returnStatement = "ret\n"
+
 
 unary :: Operator -> String
 unary o
    | o == Minus         = "neg %rax\n"
    | o == BitwiseCompl  = "not %rax\n"
    | o == LogicNegation = "cmpq $0, %rax\nmovq $0, %rax\nsete %al\n"
+
 
 binary :: String -> String -> Operator -> String
 binary loadVal1 loadVal2 o
