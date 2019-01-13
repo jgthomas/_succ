955341e4ea6b6a731a1eddc7388c9c85e7533bc2
James Thomas <codeactor@protonmail.com>
2019-01-13 20:09:27 +0000

    declaration and expressionstatement nodes now just have the variable string

--- Parser.hs
+++ Parser.hs
@@ -8,8 +8,8 @@ import Lexer
 data Tree = ProgramNode [Tree]
           | FunctionNode String [Tree]
           | ReturnNode Tree                      -- statements
-          | DeclarationNode Tree (Maybe Tree)
-          | ExpressionStatementNode Tree Tree Operator
+          | DeclarationNode String (Maybe Tree)
+          | ExpressionStatementNode String Tree Operator
           | ConstantNode Int                     -- expressions
           | VarNode String
           | UnaryNode Tree Operator
@@ -86,7 +86,7 @@ parseDeclStmt (ty:id:toks) =
                          in
                      if lookAhead toks' /= TokSemiColon
                         then error "Missing semicolon"
-                        else (DeclarationNode (VarNode varName) exprTree, accept toks')
+                        else (DeclarationNode varName exprTree, accept toks')
 
 
 parseOptionalAssign :: [Token] -> (Maybe Tree, [Token])
@@ -109,7 +109,7 @@ parseExpression toks =
                           VarNode str ->
                                   let (exTree, toks'') = parseExpression (accept toks')
                                       in
-                                  (ExpressionStatementNode expressTree exTree Assign, toks'')
+                                  (ExpressionStatementNode str exTree Assign, toks'')
                           _ -> error "Can only assign to variables"
              _ -> (expressTree, toks')
 
