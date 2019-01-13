1f6623ab45fd94643870fb84f94cee3f7149a89e
James Thomas <codeactor@protonmail.com>
2019-01-13 12:14:32 +0000

    add symtab

---
+++ SymTab.hs
@@ -0,0 +1,54 @@
+
+module SymTab (Evaluator(..), lookUp, addSymbol) where
+
+
+import Lexer
+import Parser
+import qualified Data.Map as M
+import Control.Monad (liftM, ap)
+
+
+type SymTab = M.Map String Int
+
+
+newtype Evaluator a = Ev (SymTab -> (a, SymTab))
+
+
+{-
+-  Monad is now defined as a subclass of applicative
+-  and so new monad definitions require that they also
+-  be specified as applicatives (and functors, of which
+-  applicatives are a subclass)
+-
+-  See: https://stackoverflow.com/a/34641501/5671759
+-}
+instance Functor Evaluator where
+        fmap = liftM
+
+
+instance Applicative Evaluator where
+        pure  = return
+        (<*>) = ap
+
+
+instance Monad Evaluator where
+        (Ev act) >>= k = Ev $
+            \symTab ->
+                    let (x, symTab') = act symTab
+                        (Ev act')    = k x
+                    in act' symTab'
+        return x = Ev (\symTab -> (x, symTab))
+
+
+
+lookUp :: String -> Evaluator Double
+lookUp str = Ev $ \symTab ->
+        case M.lookup str symTab of
+             Just v  -> (v, symTab)
+             Nothing -> error $ "Undefined variable: " ++ str
+
+
+addSymbol :: String -> Double -> Evaluator Double
+addSymbol str val = Ev $ \symTab ->
+        let symTab' = M.insert str val symTab
+            in (val, symTab')
