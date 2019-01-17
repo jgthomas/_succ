0598b9f972756394ed60b1bccc6234f8e288a839
James Thomas <codeactor@protonmail.com>
2019-01-17 11:11:15 +0000

    delete old code

--- Main.hs
+++ Main.hs
@@ -12,8 +12,6 @@ import Parser (parse)
 import Generator (genASM)
 import SymTab
 
---type SymTab = ()
-
 
 newSymTab :: SymTab
 newSymTab = Tab (-8) M.empty
@@ -21,12 +19,6 @@ newSymTab = Tab (-8) M.empty
 
 main :: IO()
 main = do
-        --print $ parse $ tokenize "int main() { int a; return 5; }"
-        print $ parse $ tokenize "int main() { int a = 5; return a; }"
-        print $ parse $ tokenize "int main() { int a; a = 5; return a; }"
-        --print $ parse $ tokenize "int main() { int a = 2 || 3; return 5; }"
-        --print $ parse $ tokenize "int main() { int a = 2; int b = a + 1; return b; }"
-        --print $ parse $ tokenize "int main() { int a = 2 * (7 + 3); return a; }"
         args <- getArgs
         let infileName = head args
         handle <- openFile infileName ReadMode
@@ -39,14 +31,11 @@ main = do
 
         let outfileName = (dropExtension infileName) ++ ".s"
         let parsed = parse $ tokenize contents
-        --let assembly = genASM $ parse $ tokenize contents
 
-        let symTab = newSymTab --M.empty
+        let symTab = newSymTab
             Ev act = genASM parsed
             (asm, symTab') = act symTab
             in do
-                    --writeFile outfileName asm
-
                     print symTab'
                     when (length asm > 0) $
                        writeFile outfileName asm
