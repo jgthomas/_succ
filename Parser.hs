64de7cdb3bd9926a31d16edc415c408fe05b7966
James Thomas <codeactor@protonmail.com>
2019-01-24 10:34:10 +0000

    error checking for declarations not being statements

--- Parser.hs
+++ Parser.hs
@@ -76,6 +76,7 @@ parseStatement toks =
         case lookAhead toks of
              (TokKeyword kwd) | kwd == Return -> parseReturnStmt toks
                               | kwd == If     -> parseIfStatement toks
+                              | kwd == Int    -> error "Declarations are not statements"
              TokOpenBrace                     -> parseCompoundStmt toks
              (TokIdent id) -> let (exprTree, toks') = parseExpression toks
                                   in
