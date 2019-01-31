2eb0bc11459caa6ff11c3e3dc1a5aae06892cb0c
James Thomas <codeactor@protonmail.com>
2019-01-31 10:38:30 +0000

    support iteration statement keywords in lexer

--- Lexer.hs
+++ Lexer.hs
@@ -30,6 +30,11 @@ data Keyword = Int
              | Return
              | If
              | Else
+             | For
+             | While
+             | Do
+             | Break
+             | Continue
              deriving (Show, Eq)
 
 
@@ -81,11 +86,16 @@ accept (t:ts) = ts
 identifier :: Char -> String -> [Token]
 identifier c cs = let (str, cs') = span isAlphaNum cs
                       in case (c:str) of
-                              "int"    -> TokKeyword Int    : tokenize cs'
-                              "return" -> TokKeyword Return : tokenize cs'
-                              "if"     -> TokKeyword If     : tokenize cs'
-                              "else"   -> TokKeyword Else   : tokenize cs'
-                              _        -> TokIdent (c:str)  : tokenize cs'
+                              "int"      -> TokKeyword Int       : tokenize cs'
+                              "return"   -> TokKeyword Return    : tokenize cs'
+                              "if"       -> TokKeyword If        : tokenize cs'
+                              "else"     -> TokKeyword Else      : tokenize cs'
+                              "for"      -> TokKeyword For       : tokenize cs'
+                              "while"    -> TokKeyword While     : tokenize cs'
+                              "do"       -> TokKeyword Do        : tokenize cs'
+                              "break"    -> TokKeyword Break     : tokenize cs'
+                              "continue" -> TokKeyword Continue  : tokenize cs'
+                              _          -> TokIdent (c:str)     : tokenize cs'
 
 
 number :: Char -> String -> [Token]
