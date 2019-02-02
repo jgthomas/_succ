b9bd76dd245a7fcef93857f6870ecb5fa67d9a53
James Thomas <codeactor@protonmail.com>
2019-02-02 13:24:39 +0000

    for loop debug print

--- Main.hs
+++ Main.hs
@@ -19,8 +19,7 @@ newSymTab = Tab (-1) 0 (-8) M.empty
 
 main :: IO()
 main = do
-        --print $ tokenize "int main() { int a = 2; do { a = a + 1; } while (a < 4); return a; }"
-        print $ parse $ tokenize "int main() { int a = 2; do { a = a + 1; } while (a < 4); return a; }"
+        print $ tokenize "int main() { int a = 2; for (int i = 0; i < 3; i = i + 1) { a = a + i; } return a; }"
         args <- getArgs
         let infileName = head args
         handle <- openFile infileName ReadMode
