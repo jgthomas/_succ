d1142c0f54ff094cd2d502a7a8968f3ef2bfeccf
James Thomas <codeactor@protonmail.com>
2019-04-10 07:30:54 +0100

    description added of state objects

--- SymTab.hs
+++ SymTab.hs
@@ -45,6 +45,14 @@ type FuncParams = M.Map String Int
 type Declarations = O.OMap String Int
 
 
+{-
+- State of a function
+-
+- paramCount : the number of parameters the function has
+- argCount   : counter for arguments passed
+- parameters : key=parameter name, value=parameter position
+-
+-}
 data FuncState = Fs { paramCount :: Int
                     , argCount   :: Int
                     , parameters :: FuncParams }
@@ -54,6 +62,21 @@ data FuncState = Fs { paramCount :: Int
 type FuncStates = M.Map String FuncState
 
 
+{-
+- State of the whole program
+-
+- labelNo      : the number of the next label to output in asm
+- offset       : the offset from %rbp where the next local variable should be stored
+- nameStack    : stack of program function calls, current function at top
+- declarations : key=function name, value=number indicating sequence of when declared
+- funcStates   : key=function name, value=state container for that function
+- scopeLevels  : key=function name, value=current scope depth in that function
+- scopesData   : triply nested map tracking the variables in each scope
+-    ProgramScope  : key=function name, value=map of the scopes in each function
+-    FunctionScope : key=scope level in function, value=map of variables in that scope
+-    LocalScope    : key=variable name, value=offset from %rbp where stored
+-
+-}
 data SymTab = Tab { labelNo      :: Int
                   , offset       :: Int
                   , nameStack    :: Stack String
