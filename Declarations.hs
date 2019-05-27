956f903ebe9593a2516ae9d69afe1a8870ae314b
James Thomas <codeactor@protonmail.com>
2019-05-27 10:45:05 +0100

    add initial declaration tracking module

---
+++ Declarations.hs
@@ -0,0 +1,45 @@
+
+module Declarations (Declaration(..),
+                     newDecTable)
+        where
+
+
+import Data.Map as M
+
+
+type SeqNums = M.Map String Int
+type ParamCounts = M.Map String Int
+
+
+data Declaration = SymbolSeq SeqNums
+                 | ParamCount ParamCounts
+                 deriving Show
+
+
+data Declared = D { declOrder :: Declaration
+                  , parameter :: Declaration }
+              deriving Show
+
+
+newDecTable :: Declared
+newDecTable = D
+              (SymbolSeq M.empty)
+              (ParamCount M.empty)
+
+
+isDeclared :: Declared -> String -> Bool
+isDeclared table name =
+        let seqTab = declOrder table
+            in
+        checkDeclared seqTab name
+
+
+checkDeclared :: Declaration -> String -> Bool
+checkDeclared (SymbolSeq seq) name = lookUp seq name
+
+
+lookUp :: SeqNums -> String -> Bool
+lookUp table name =
+        case M.lookup name table of
+             Just n  -> True
+             Nothing -> False
