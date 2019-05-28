a5668eef908176f22e45f71ac3fb28013ce306ad
James Thomas <codeactor@protonmail.com>
2019-05-28 08:35:24 +0100

    comment out

--- SymbolTable.hs
+++ SymbolTable.hs
@@ -1,40 +1,40 @@
 
-module SymbolTable (SymTab(..)) where
-
-
-import qualified Data.Map as M
-
-import SimpleStack (Stack)
-import FunctionState (FuncStates)
-import Scopes (ProgramScope)
-import Declarations (Declared)
-
-
-{-
-- State of the whole program
--
-- labelNo      : the number of the next label to output in asm
-- offset       : the offset from %rbp where the next local variable should be stored
-- nameStack    : stack of program function calls, current function at top
-- declarations : key=function name, value=number indicating sequence of when declared
-- funcStates   : key=function name, value=state container for that function
-- scopeLevels  : key=function name, value=current scope depth in that function
-- scopesData   : triply nested map tracking the variables in each scope
--    ProgramScope
--          key=function name, value=map of the scopes in each function
--    FunctionScope
--          key=scope level in function, value=map of variables in that scope
--    LocalScope
--          key=variable name, value=offset from %rbp where stored
--
--}
-data SymTab = Tab { labelNo      :: Int
-                  , offset       :: Int
-                  , nameStack    :: Stack String
-                  , declarations :: Declared
-                  , funcStates   :: FuncStates
-                  , scopeLevels  :: M.Map String Int
-                  , scopesData   :: ProgramScope }
-            deriving Show
-
+--module SymbolTable (SymTab(..)) where
+--
+--
+--import qualified Data.Map as M
+--
+--import SimpleStack (Stack)
+--import FunctionState (FuncStates)
+--import Scopes (ProgramScope)
+--import Declarations (Declared)
+--
+--
+--{-
+--- State of the whole program
+---
+--- labelNo      : the number of the next label to output in asm
+--- offset       : the offset from %rbp where the next local variable should be stored
+--- nameStack    : stack of program function calls, current function at top
+--- declarations : key=function name, value=number indicating sequence of when declared
+--- funcStates   : key=function name, value=state container for that function
+--- scopeLevels  : key=function name, value=current scope depth in that function
+--- scopesData   : triply nested map tracking the variables in each scope
+---    ProgramScope
+---          key=function name, value=map of the scopes in each function
+---    FunctionScope
+---          key=scope level in function, value=map of variables in that scope
+---    LocalScope
+---          key=variable name, value=offset from %rbp where stored
+---
+---}
+--data SymTab = Tab { labelNo      :: Int
+--                  , offset       :: Int
+--                  , nameStack    :: Stack String
+--                  , declarations :: Declared
+--                  , funcStates   :: FuncStates
+--                  , scopeLevels  :: M.Map String Int
+--                  , scopesData   :: ProgramScope }
+--            deriving Show
+--
 
