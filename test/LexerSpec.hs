b46a14753c8caae5ed93193da73c9a21413c916a
James Thomas <codeactor@protonmail.com>
2019-06-07 10:06:55 +0100

    add two further tests of the lexer

--- test/LexerSpec.hs
+++ test/LexerSpec.hs
@@ -13,3 +13,8 @@ main = hspec $ do
                 it "simple token of a single variable" $
                   tokenize "int a;" `shouldBe` [TokKeyword Int,TokIdent "a",TokSemiColon]
 
+                it "should be a two character operator then a single one" $
+                  tokenize "+=+" `shouldBe` [TokOp PlusAssign,TokOp Plus]
+
+                it "should be a two character operator then a single one" $
+                  tokenize "+==" `shouldBe` [TokOp PlusAssign,TokOp Plus]
