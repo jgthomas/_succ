8abafc0c59bc2273fcb820afd295d43de3004dbf
James Thomas <codeactor@protonmail.com>
2019-06-07 15:29:59 +0100

    separate file with the test in from the main test running module

--- test/LexerSpec.hs
+++ test/LexerSpec.hs
@@ -1,5 +1,5 @@
 
-module LexerSpec where
+module LexerSpec (lexerTest) where
 
 
 import Test.Hspec
@@ -7,14 +7,17 @@ import Lexer
 import Tokens
 
 
-main :: IO ()
-main = hspec $ do
+lexerTest :: IO ()
+lexerTest = hspec $ do
         describe "lexes tokens" $ do
                 it "simple token of a single variable" $
                   tokenize "int a;" `shouldBe` [TokKeyword Int,TokIdent "a",TokSemiColon]
 
-                it "should be a two character operator then a single one" $
+                it "should be a two character operator then a single one I" $
                   tokenize "+=+" `shouldBe` [TokOp PlusAssign,TokOp Plus]
 
-                it "should be a two character operator then a single one" $
+                it "should be a two character operator then a single one II" $
                   tokenize "+==" `shouldBe` [TokOp PlusAssign,TokOp Assign]
+
+                it "should be two of the SAME two-character operators" $
+                  tokenize "+=+=" `shouldBe` [TokOp PlusAssign,TokOp PlusAssign]
