964f980d0057e3605027c899f9f8db577559ec4f
James Thomas <codeactor@protonmail.com>
2019-06-23 17:08:26 +0100

    remove all reference to global from FuncState

--- FuncState.hs
+++ FuncState.hs
@@ -96,11 +96,7 @@ variableOffset name = getOffset name
 
 
 variableType :: String -> Evaluator (Maybe Type)
-variableType name = do
-        currScope <- FrameStack.currentFunction
-        if currScope == "global"
-           then return Nothing
-           else getType name
+variableType name = getType name
 
 
 addVariable :: String -> Type -> Evaluator Int
@@ -132,11 +128,7 @@ parameterPosition paramName = do
 parameterType :: String -> Evaluator (Maybe Type)
 parameterType paramName = do
         currFuncName <- FrameStack.currentFunction
-        if currFuncName == "global"
-           then return Nothing
-           else extract paramType
-                . M.lookup paramName
-                . parameters <$> getFunctionState currFuncName
+        extract paramType . M.lookup paramName . parameters <$> getFunctionState currFuncName
 
 
 allTypes :: String -> Evaluator [Type]
