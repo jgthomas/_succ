1d63901277b9aabd7a33241dc290a978439b2c79
James Thomas <codeactor@protonmail.com>
2019-10-26 18:21:56 +0100

    comment variable

--- func_test.sh
+++ func_test.sh
@@ -2,7 +2,7 @@
 
 SUCC_BIN=$(stack exec which succ-exe)
 
-TEST_DIR=$HOME/Code/write_a_c_compiler
+TEST_DIR="$HOME/Code/write_a_c_compiler"
 TEST_SCRIPT="./test_compiler.sh"
 TEST_CASES="1 2 3 4 5 6 7 8 9 10 ops pointers types"
 
