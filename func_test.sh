7a8d1becc643de9dccf83728ebd4ccb4bc9238d8
James Thomas <codeactor@protonmail.com>
2019-10-26 18:19:32 +0100

    add script to run functional tests

---
+++ func_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SUCC_BIN=$(stack exec which succ-exe)
+
+TEST_DIR=$HOME/Code/write_a_c_compiler
+TEST_SCRIPT="./test_compiler.sh"
+
+TEST_CASES="1 2 3 4 5 6 7 8 9 10 ops pointers types"
+
+(
+export SUCC_BIN &&
+        cd $TEST_DIR &&
+        $TEST_SCRIPT $SUCC_BIN $TEST_CASES
+)
