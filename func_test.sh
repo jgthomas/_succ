5ba4fb025275ad79687b484c99946ab70a3d779c
James Thomas <codeactor@protonmail.com>
2019-10-27 08:07:49 +0000

    break bin out into own var

--- func_test.sh
+++ func_test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-SUCC_BIN=$(stack exec which succ-exe)
+SUCC_BIN="succ-exe"
+SUCC_BIN_PATH=$(stack exec which $SUCC_BIN)
 
 TEST_DIR="$HOME/Code/write_a_c_compiler"
 TEST_SCRIPT="./test_compiler.sh"
 TEST_CASES="1 2 3 4 5 6 7 8 9 10 ops pointers types"
 
-(cd $TEST_DIR && $TEST_SCRIPT $SUCC_BIN $TEST_CASES)
+(cd $TEST_DIR && $TEST_SCRIPT $SUCC_BIN_PATH $TEST_CASES)
