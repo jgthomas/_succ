e1e3308594226a1e352a3c8127e49aa0980909c0
James Thomas <codeactor@protonmail.com>
2019-10-27 09:34:54 +0000

    place each test stage folder on own line

--- func_test.sh
+++ func_test.sh
@@ -5,6 +5,18 @@ SUCC_BIN_PATH=$(stack exec which $SUCC_BIN)
 
 TEST_DIR="$HOME/Code/write_a_c_compiler"
 TEST_SCRIPT="./test_compiler.sh"
-TEST_CASES="1 2 3 4 5 6 7 8 9 10 ops pointers types"
+TEST_CASES="1 \
+        2 \
+        3 \
+        4 \
+        5 \
+        6 \
+        7 \
+        8 \
+        9 \
+        10 \
+        ops \
+        pointers \
+        types"
 
 (cd $TEST_DIR && $TEST_SCRIPT $SUCC_BIN_PATH $TEST_CASES)
