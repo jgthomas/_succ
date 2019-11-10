48f93433e9cc6a41f685c36b430f5486ec13119c
James Thomas <codeactor@protonmail.com>
2019-11-10 19:13:31 +0000

    align default test cases

--- func_test.sh
+++ func_test.sh
@@ -8,18 +8,18 @@ SUCC_BIN_PATH=$(stack exec which $SUCC_BIN)
 TEST_DIR="$HOME/Code/write_a_c_compiler"
 TEST_SCRIPT="./test_compiler.sh"
 ALL_TEST_CASES="1 \
-        2 \
-        3 \
-        4 \
-        5 \
-        6 \
-        7 \
-        8 \
-        9 \
-        10 \
-        ops \
-        pointers \
-        types"
+                2 \
+                3 \
+                4 \
+                5 \
+                6 \
+                7 \
+                8 \
+                9 \
+                10 \
+                ops \
+                pointers \
+                types"
 
 
 if [[ ! -z $TESTS ]]; then
