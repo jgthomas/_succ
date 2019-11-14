d95ef48bb327f91744bd6aef83d2ceac7bea216b
James Thomas <codeactor@protonmail.com>
2019-11-14 19:31:28 +0000

    Remove unused tokens

--- src/Tokens.hs
+++ src/Tokens.hs
@@ -55,7 +55,5 @@ data Token = TokOpenParen
            | TokQuestMark
            | TokComma
            | TokSpace
-           | TokUnrecognised
-           | TokNull
            | TokWut
            deriving (Show, Eq)
