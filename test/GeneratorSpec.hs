33e9fd38d8ebb3b1c9faa993f6b45b99dd22de65
James Thomas <codeactor@protonmail.com>
2019-12-14 15:24:57 +0000

    First unit test for generator

---
+++ test/GeneratorSpec.hs
@@ -0,0 +1,39 @@
+
+module GeneratorSpec (generatorTest) where
+
+
+import Data.Either
+import Test.Hspec
+
+import Generator
+import Tokens
+import AST
+import Error
+import VarTypes
+
+
+generatorTest :: IO ()
+generatorTest = hspec $ do
+        describe "Build output string from AST" $ do
+                it "Should make asm for valid AST" $
+                  fromRight "FAIL" (generate (ProgramNode
+                                              [FunctionNode IntVar "main" []
+                                               (Just [ReturnNode (ConstantNode 2)])
+                                              ]
+                                             ))
+                  `shouldBe`
+                  unlines [".globl main",
+                           "main:",
+                           "jmp init",
+                           "init_done:",
+                           "pushq %rbp",
+                           "movq %rsp, %rbp",
+                           "pushq %r12",
+                           "movq $2, %rax",
+                           "popq %r12",
+                           "movq %rbp, %rsp",
+                           "popq %rbp",
+                           "ret",
+                           "init:",
+                           "jmp init_done"
+                          ]
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,9 +4,11 @@ module Main where
 
 import LexerSpec
 import ParserSpec
+import GeneratorSpec
 
 
 main :: IO ()
 main = do
         lexerTest
         parserTest
+        generatorTest
