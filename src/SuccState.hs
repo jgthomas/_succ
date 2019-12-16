3335f4c08b79ca3b440e71da569849343a5d8494
James Thomas <codeactor@protonmail.com>
2019-12-16 21:06:15 +0000

    Haddock docs

--- src/SuccState.hs
+++ src/SuccState.hs
@@ -1,3 +1,10 @@
+{-|
+Module       : SuccState
+Description  : Core compiler state definition
+
+Defines the core monad transformer stack used in the compliler, combining
+a State monad with an error type.
+-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module SuccState
@@ -15,22 +22,27 @@ import Control.Monad.Trans.Except (ExceptT, runExceptT, throwE)
 import Error                      (CompilerError)
 
 
+-- | Wrapper type for the compiler state
 newtype SuccStateM s a = CM {
         unCM :: ExceptT CompilerError (State s) a
 } deriving (Functor, Applicative, Monad)
 
 
+-- | Extract the contents of the state
 getState :: SuccStateM a a
 getState = CM (lift get)
 
 
+-- | Update the contents of the state
 putState :: s -> SuccStateM s ()
 putState s = CM $ lift $ put s
 
 
+-- | Throw an error
 throwError :: CompilerError -> SuccStateM s a
 throwError e = CM $ throwE e
 
 
+-- | Run the state extracting the error or result
 runSuccState :: (t -> SuccStateM s a) -> t -> s -> Either CompilerError a
 runSuccState f t s = evalState (runExceptT . unCM $ f t) s
