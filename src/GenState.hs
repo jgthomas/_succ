e9c0bea0b52be58e3dc249d6a930199362304ae1
James Thomas <codeactor@protonmail.com>
2019-12-17 20:10:17 +0000

    Haddock docs

--- src/GenState.hs
+++ src/GenState.hs
@@ -1,4 +1,9 @@
+{-|
+Module       : GenState
+Description  : State for the code generator
 
+State holder for the code generation stage of compilation.
+-}
 module GenState
         (GenState,
          mkSymTab,
@@ -27,49 +32,58 @@ data SymTab = Tab { label       :: Int
             deriving (Show)
 
 
+-- | State definition
 type GenState = SuccStateM SymTab
 
 
+-- | State constructor
 mkSymTab :: SymTab
 mkSymTab = Tab 1 mkStack mkGlobalScope M.empty
 
 
+-- | Get the global scope state holder
 getGlobalScope :: GenState GlobalScope
 getGlobalScope = do
         state <- getState
         pure . globalScope $ state
 
 
+-- | Update the global scope state holder
 putGlobalScope :: GlobalScope -> GenState ()
 putGlobalScope gs = do
         state <- getState
         putState $ state { globalScope = gs }
 
 
+-- | Get the function scope state holder
 getFuncStates :: GenState (M.Map String FuncState)
 getFuncStates = do
         state <- getState
         pure . funcStates $ state
 
 
+-- | Update the function scope state holder
 putFuncStates :: M.Map String FuncState -> GenState ()
 putFuncStates fs = do
         state <- getState
         putState $ state { funcStates = fs }
 
 
+-- | Get the framestack
 getFrameStack :: GenState (Stack String)
 getFrameStack = do
         state <- getState
         pure . frameStack $ state
 
 
+-- | Update the framestack
 putFrameStack :: Stack String -> GenState ()
 putFrameStack stack = do
         state <- getState
         putState $ state { frameStack = stack }
 
 
+-- | Get label number, incrementing the state
 labelNum :: GenState Int
 labelNum = do
         l <- getLabel
