46e52817049cf9c884ddaf8af6e357f500899182
James Thomas <codeactor@protonmail.com>
2019-12-21 19:46:04 +0000

    Now returns pair of token and string

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -37,100 +37,103 @@ lexInput :: String -> LexerState [Token]
 lexInput [] = do
         lexOut <- getState
         pure . reverse $ lexOut
-lexInput input@(c:cs) =
-        if | isSeparator c    -> separator input
-           | isTwoCharOp c cs -> twoCharOperator input
-           | isOpSymbol c     -> operator input
-           | identStart c     -> identifier input
-           | isDigit c        -> number input
-           | isSpace c        -> lexInput cs
-           | otherwise        -> throwError (LexerError (BadInput [c]))
+lexInput input@(c:cs)
+        | isSpace c = lexInput cs
+        | otherwise = do
+                lexOut <- getState
+                (tok, input') <- lexToken input
+                putState (tok:lexOut)
+                lexInput input'
 
 
-separator :: String -> LexerState [Token]
+lexToken :: String -> LexerState (Token, String)
+lexToken [] = throwError ImpossibleError
+lexToken input@(c:cs)
+        | isSeparator c    = separator input
+        | isTwoCharOp c cs = twoCharOperator input
+        | isOpSymbol c     = operator input
+        | identStart c     = identifier input
+        | isDigit c        = number input
+        | otherwise        = throwError (LexerError (BadInput [c]))
+
+
+separator :: String -> LexerState (Token, String)
 separator [] = throwError ImpossibleError
 separator (c:cs) =
         case c of
-             '(' -> updateState OpenParen cs
-             ')' -> updateState CloseParen cs
-             '{' -> updateState OpenBrace cs
-             '}' -> updateState CloseBrace cs
-             ';' -> updateState SemiColon cs
-             ':' -> updateState Colon cs
-             '?' -> updateState QuestMark cs
-             ',' -> updateState Comma cs
+             '(' -> pure (OpenParen, cs)
+             ')' -> pure (CloseParen, cs)
+             '{' -> pure (OpenBrace, cs)
+             '}' -> pure (CloseBrace, cs)
+             ';' -> pure (SemiColon, cs)
+             ':' -> pure (Colon, cs)
+             '?' -> pure (QuestMark, cs)
+             ',' -> pure (Comma, cs)
              _   -> throwError $ LexerError (UnexpectedInput [c])
 
 
-identifier :: String -> LexerState [Token]
+identifier :: String -> LexerState (Token, String)
 identifier [] = throwError ImpossibleError
 identifier (c:cs) =
         let (str, cs') = span isValidInIdentifier cs
             in
         case c:str of
-             "int"      -> updateState (Keyword Int) cs'
-             "return"   -> updateState (Keyword Return) cs'
-             "if"       -> updateState (Keyword If) cs'
-             "else"     -> updateState (Keyword Else) cs'
-             "for"      -> updateState (Keyword For) cs'
-             "while"    -> updateState (Keyword While) cs'
-             "do"       -> updateState (Keyword Do) cs'
-             "break"    -> updateState (Keyword Break) cs'
-             "continue" -> updateState (Keyword Continue) cs'
-             _          -> updateState (Ident (c:str)) cs'
+             "int"      -> pure (Keyword Int, cs')
+             "return"   -> pure (Keyword Return, cs')
+             "if"       -> pure (Keyword If, cs')
+             "else"     -> pure (Keyword Else, cs')
+             "for"      -> pure (Keyword For, cs')
+             "while"    -> pure (Keyword While, cs')
+             "do"       -> pure (Keyword Do, cs')
+             "break"    -> pure (Keyword Break, cs')
+             "continue" -> pure (Keyword Continue, cs')
+             _          -> pure (Ident (c:str), cs')
 
 
-number :: String -> LexerState [Token]
+number :: String -> LexerState (Token, String)
 number [] = throwError ImpossibleError
 number (c:cs) =
         let (digs, cs') = span isDigit cs
             tok         = ConstInt (read (c:digs))
             in
-        updateState tok cs'
+        pure (tok, cs')
 
 
-twoCharOperator :: String -> LexerState [Token]
+twoCharOperator :: String -> LexerState (Token, String)
 twoCharOperator []  = throwError ImpossibleError
 twoCharOperator [_] = throwError ImpossibleError
 twoCharOperator (c:n:cs) =
         case c:[n] of
-             "||" -> updateState (OpTok PipePipe) cs
-             "&&" -> updateState (OpTok AmpAmp) cs
-             ">=" -> updateState (OpTok RightArrowEqual) cs
-             "<=" -> updateState (OpTok LeftArrowEqual) cs
-             "==" -> updateState (OpTok EqualEqual) cs
-             "!=" -> updateState (OpTok BangEqual) cs
-             "+=" -> updateState (OpTok PlusEqual) cs
-             "-=" -> updateState (OpTok MinusEqual) cs
-             "*=" -> updateState (OpTok AsteriskEqual) cs
-             "/=" -> updateState (OpTok BackslashEqual) cs
-             "%=" -> updateState (OpTok PercentEqual) cs
+             "||" -> pure (OpTok PipePipe, cs)
+             "&&" -> pure (OpTok AmpAmp, cs)
+             ">=" -> pure (OpTok RightArrowEqual, cs)
+             "<=" -> pure (OpTok LeftArrowEqual, cs)
+             "==" -> pure (OpTok EqualEqual, cs)
+             "!=" -> pure (OpTok BangEqual, cs)
+             "+=" -> pure (OpTok PlusEqual, cs)
+             "-=" -> pure (OpTok MinusEqual, cs)
+             "*=" -> pure (OpTok AsteriskEqual, cs)
+             "/=" -> pure (OpTok BackslashEqual, cs)
+             "%=" -> pure (OpTok PercentEqual, cs)
              _    -> throwError $ LexerError (UnexpectedInput (c:[n]))
 
 
-operator :: String -> LexerState [Token]
+operator :: String -> LexerState (Token, String)
 operator [] = throwError ImpossibleError
 operator (c:cs) =
         case c of
-             '+' -> updateState (OpTok PlusSign) cs
-             '-' -> updateState (OpTok MinusSign) cs
-             '*' -> updateState (OpTok Asterisk) cs
-             '%' -> updateState (OpTok Percent) cs
-             '/' -> updateState (OpTok BackSlash) cs
-             '~' -> updateState (OpTok Tilde) cs
-             '!' -> updateState (OpTok Bang) cs
-             '>' -> updateState (OpTok RightArrow) cs
-             '<' -> updateState (OpTok LeftArrow) cs
-             '=' -> updateState (OpTok EqualSign) cs
-             '&' -> updateState (OpTok Ampersand) cs
+             '+' -> pure (OpTok PlusSign, cs)
+             '-' -> pure (OpTok MinusSign, cs)
+             '*' -> pure (OpTok Asterisk, cs)
+             '%' -> pure (OpTok Percent, cs)
+             '/' -> pure (OpTok BackSlash, cs)
+             '~' -> pure (OpTok Tilde, cs)
+             '!' -> pure (OpTok Bang, cs)
+             '>' -> pure (OpTok RightArrow, cs)
+             '<' -> pure (OpTok LeftArrow, cs)
+             '=' -> pure (OpTok EqualSign, cs)
+             '&' -> pure (OpTok Ampersand, cs)
              _   -> throwError $ LexerError (UnexpectedInput [c])
-
-
-updateState :: Token -> String -> LexerState [Token]
-updateState tok input = do
-        lexOut <- getState
-        putState (tok:lexOut)
-        lexInput input
 
 
 isSeparator :: Char -> Bool
