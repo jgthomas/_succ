680bd08f8c775ff44cb49602ac7560c4c539e6a0
James Thomas <codeactor@protonmail.com>
2020-01-01 16:30:31 +0000

    Reformat all test lines

--- test/LexerSpec.hs
+++ test/LexerSpec.hs
@@ -54,22 +54,29 @@ lexerTest = hspec $ do
 
 
                 it "simple token of a single variable" $
-                  fromRight [] (tokenize "int a;") `shouldBe` [Keyword Int,Ident "a",SemiColon]
+                  fromRight [] (tokenize "int a;")
+                  `shouldBe` [Keyword Int,Ident "a",SemiColon]
 
                 it "should be a two-character operator then a single one I" $
-                  fromRight [] (tokenize "+=+") `shouldBe` [OpTok PlusEqual,OpTok PlusSign]
+                  fromRight [] (tokenize "+=+")
+                  `shouldBe` [OpTok PlusEqual,OpTok PlusSign]
 
                 it "should be a two character operator then a single one II" $
-                  fromRight [] (tokenize "+==") `shouldBe` [OpTok PlusEqual,OpTok EqualSign]
+                  fromRight [] (tokenize "+==")
+                  `shouldBe` [OpTok PlusEqual,OpTok EqualSign]
 
                 it "should be two of the SAME two-character operators" $
-                  fromRight [] (tokenize "+=+=") `shouldBe` [OpTok PlusEqual,OpTok PlusEqual]
+                  fromRight [] (tokenize "+=+=")
+                  `shouldBe` [OpTok PlusEqual,OpTok PlusEqual]
 
                 it "should throw error for unrecognised character" $
-                  fromLeft ImpossibleError (tokenize "$") `shouldBe` LexerError (BadInput "$")
+                  fromLeft ImpossibleError (tokenize "$")
+                  `shouldBe` LexerError (BadInput "$")
 
                 it "should throw error for empty input" $
-                  fromLeft ImpossibleError (tokenize "") `shouldBe` LexerError EmptyInput
+                  fromLeft ImpossibleError (tokenize "")
+                  `shouldBe` LexerError EmptyInput
 
                 it "should lex the caret operator" $
-                  fromRight [] (tokenize "^") `shouldBe` [OpTok Caret]
+                  fromRight [] (tokenize "^")
+                  `shouldBe` [OpTok Caret]
