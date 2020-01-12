20b78c808c88faddd8b390de45cccd110133ab65
James Thomas <codeactor@protonmail.com>
2020-01-12 10:09:33 +0000

    Map checker over all items

--- src/Checker.hs
+++ src/Checker.hs
@@ -2,7 +2,7 @@
 module Checker (check) where
 
 
-import           AST      (Tree)
+import           AST      (Tree (..))
 import           Error    (CompilerError)
 import           GenState (GenState, runGenState)
 import qualified GenState (startState)
@@ -13,4 +13,9 @@ check ast = runGenState checkAST ast GenState.startState
 
 
 checkAST :: Tree -> GenState Tree
+
+checkAST ast@(ProgramNode topLevelItems) = do
+        mapM_ checkAST topLevelItems
+        pure ast
+
 checkAST ast = pure ast
