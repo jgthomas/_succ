438020daf7169a7eb31c626785c022bd0a142d54
James Thomas <codeactor@protonmail.com>
2020-02-09 22:32:18 +0000

    Move initialisation asm to directive

--- src/ASM.hs
+++ src/ASM.hs
@@ -13,11 +13,9 @@ module ASM
          module AsmStatement,
          module AsmFuncCall,
          module AsmVariables,
+         module Directive,
          loadLiteral,
          setGotoPoint,
-         initializedGlobal,
-         uninitializedGlobal,
-         outputInit,
          noOutput
         ) where
 
@@ -32,10 +30,10 @@ import AsmStatement (doWhile, forLoop, ifElse, ifOnly, while)
 import AsmTernary   (ternary)
 import AsmUnary     (unary)
 import AsmVariables (assign, decNoAssign, loadVariable, storeGlobal)
-import Assembly     (Jump (..), Section (..))
-import Directive
+import Assembly     (Jump (..))
+import Directive    (initializedGlobal, outputInit, uninitializedGlobal)
 import GenState     (GenState)
-import Instruction
+import Instruction  (emitJump)
 
 
 -- | Output asm for jump
@@ -46,32 +44,6 @@ setGotoPoint target = emitJump JMP target
 -- | Load a literal value into return register
 loadLiteral :: Int -> GenState String
 loadLiteral n = pure . loadValue $ n
-
-
--- | Output asm for an initialized global variable
-initializedGlobal :: String -> String -> GenState String
-initializedGlobal label val = pure $
-        declareGlobl label
-        ++ section DATA
-        ++ align
-        ++ globlLabel label
-        ++ asLong val
-        ++ section TEXT
-
-
--- | Output asm for an uninitialized global variable
-uninitializedGlobal :: String -> GenState String
-uninitializedGlobal label = pure $
-        declareGlobl label
-        ++ section BSS
-        ++ align
-        ++ globlLabel label
-        ++ section TEXT
-
-
--- | Setup initialisation block
-outputInit :: String -> String
-outputInit toInit = "init:\n" ++ toInit ++ "jmp init_done\n"
 
 
 -- | Empty output
--- src/Directive.hs
+++ src/Directive.hs
@@ -3,6 +3,33 @@ module Directive where
 
 
 import Assembly (Section (..))
+import GenState (GenState)
+
+
+-- | Setup initialisation block
+outputInit :: String -> String
+outputInit toInit = "init:\n" ++ toInit ++ "jmp init_done\n"
+
+
+-- | Output asm for an initialized global variable
+initializedGlobal :: String -> String -> GenState String
+initializedGlobal label val = pure $
+        declareGlobl label
+        ++ section DATA
+        ++ align
+        ++ globlLabel label
+        ++ asLong val
+        ++ section TEXT
+
+
+-- | Output asm for an uninitialized global variable
+uninitializedGlobal :: String -> GenState String
+uninitializedGlobal label = pure $
+        declareGlobl label
+        ++ section BSS
+        ++ align
+        ++ globlLabel label
+        ++ section TEXT
 
 
 declareGlobl :: String -> String
