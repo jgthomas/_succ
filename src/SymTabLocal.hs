45b4a1905ca13f81f9b5e462922c5da3b79feaf8
James Thomas <codeactor@protonmail.com>
2020-02-15 09:48:55 +0000

    Break functions for manipulating function variables into own module

--- src/SymTabLocal.hs
+++ src/SymTabLocal.hs
@@ -6,45 +6,26 @@ Functions to manipulate the state stored for each function being
 compiled.
 -}
 module SymTabLocal
-        (initScope,
-         closeScope,
+        (module SymTabLocalOffset,
+         module SymTabLocalVars,
+         module SymTabLocalScope,
          initFunction,
          closeFunction,
-         getBreak,
-         setBreak,
-         getContinue,
-         setContinue,
-         checkVariable,
-         variableOffset,
-         variableType,
-         addParameter,
-         parameterPosition,
-         parameterType,
-         parameterDeclared,
-         addVariable,
-         delFuncState,
-         allTypes,
-         module SymTabLocalOffset
+         delFuncState
         ) where
 
 
 import           Control.Monad     (when)
-import           Data.Function     (on)
-import           Data.List         (sortBy)
-import qualified Data.Map          as M
 import           Data.Maybe        (isNothing)
 
-import qualified FrameStack        (currentFunc, popFunc, pushFunc)
+import qualified FrameStack        (popFunc, pushFunc)
 import           GenState          (GenState)
 import qualified GenState          (getFuncState)
-import           GenStateLocal     (FuncState (..), LocalVar (..),
-                                    ParamVar (..))
-import qualified GenStateLocal     (mkFuncState, mkLocVar, mkParVar)
-import           SymTabLocalOffset (currentOffset, incrementOffsetByN,
-                                    stackPointerValue)
-import           SymTabLocalScope  (closeScope, getScope, initScope, scopeDepth)
-import           SymTabLocalShared (delFuncState, getFuncState, setFuncState)
-import           Type              (Type (Label))
+import qualified GenStateLocal     (mkFuncState)
+import           SymTabLocalOffset (stackPointerValue)
+import           SymTabLocalScope  (closeScope, initScope)
+import           SymTabLocalShared (delFuncState, setFuncState)
+import           SymTabLocalVars
 
 
 -- | Switch to scope of named function, creating if needed
@@ -59,164 +40,3 @@ initFunction name = do
 -- | Close current function scope
 closeFunction :: GenState ()
 closeFunction = FrameStack.popFunc
-
-
--- | Get the break label number for current scope
-getBreak :: GenState (Maybe Int)
-getBreak = getAttribute locOffset "@Break"
-
-
--- | Get the continue label number for current scope
-getContinue :: GenState (Maybe Int)
-getContinue = getAttribute locOffset "@Continue"
-
-
--- | Set the break label number for current scope
-setBreak :: Int -> GenState ()
-setBreak labelNo = store "@Break" labelNo Label
-
-
--- | Set the continue label number for current scope
-setContinue :: Int -> GenState ()
-setContinue labelNo = store "@Continue" labelNo Label
-
-
--- | Check if variable name is in use in current scope
-checkVariable :: String -> GenState Bool
-checkVariable varName = do
-        fName  <- FrameStack.currentFunc
-        scope  <- scopeDepth fName
-        locVar <- getLocalVar fName scope varName
-        case locVar of
-             Just _  -> pure True
-             Nothing -> pure False
-
-
--- | Get the offset from base pointer of variable
-variableOffset :: String -> GenState (Maybe Int)
-variableOffset name = getAttribute locOffset name
-
-
--- | Get the type of variable
-variableType :: String -> GenState (Maybe Type)
-variableType name = getAttribute locType name
-
-
--- | Store new variable, returning offset from base pointer
-addVariable :: String -> Type -> GenState Int
-addVariable varName typ = do
-        currOff <- currentOffset
-        store varName currOff typ
-        incrementOffsetByN 1
-        pure currOff
-
-
--- | Add a new parameter to the state of a function
-addParameter :: String -> Type -> GenState ()
-addParameter paramName typ = do
-        currFuncName <- FrameStack.currentFunc
-        funcState    <- getFuncState currFuncName
-        let funcState' = addParam paramName typ funcState
-        setFuncState currFuncName funcState'
-
-
--- | Retrieve the position of function parameter
-parameterPosition :: String -> GenState (Maybe Int)
-parameterPosition paramName = do
-        funcName <- FrameStack.currentFunc
-        getParamPos paramName funcName
-
-
--- | Retrieve the type of function parameter
-parameterType :: String -> GenState (Maybe Type)
-parameterType paramName = do
-        currFuncName <- FrameStack.currentFunc
-        extract paramType
-            . M.lookup paramName
-            . parameters <$> getFuncState currFuncName
-
-
--- | Retrieve list of all the type of function parameters
-allTypes :: String -> GenState [Type]
-allTypes funcName = do
-        paramList <- M.elems . parameters <$> getFuncState funcName
-        pure $ snd <$> sortBy (compare `on` fst) (paramData <$> paramList)
-
-
--- | Check a parameter exits for function
-parameterDeclared :: String -> GenState Bool
-parameterDeclared paramName = do
-        pos <- parameterPosition paramName
-        case pos of
-             Just _  -> pure True
-             Nothing -> pure False
-
-
--- store and lookup
-
-getAttribute :: (LocalVar -> a) -> String -> GenState (Maybe a)
-getAttribute f varName = do
-        funcName <- FrameStack.currentFunc
-        getAttr f varName funcName
-
-
-getAttr :: (LocalVar -> a) -> String -> String -> GenState (Maybe a)
-getAttr _ _ "global" = pure Nothing
-getAttr f varName funcName = do
-        scopeLevel <- scopeDepth funcName
-        extract f <$> find funcName scopeLevel varName
-
-
-find :: String -> Int -> String -> GenState (Maybe LocalVar)
-find _ (-1) _ = pure Nothing
-find funcName scope name = do
-        locVar <- getLocalVar funcName scope name
-        case locVar of
-             Nothing -> find funcName (pred scope) name
-             Just lv -> pure (Just lv)
-
-
-store :: String -> Int -> Type -> GenState ()
-store name value typ = do
-        funcName <- FrameStack.currentFunc
-        fstate   <- getFuncState funcName
-        let level = currentScope fstate
-        scope <- getScope level fstate
-        let locVar  = GenStateLocal.mkLocVar value typ
-            scope'  = M.insert name locVar scope
-            fstate' = fstate { scopes = M.insert level scope' $ scopes fstate }
-        setFuncState funcName fstate'
-
-
-getLocalVar :: String -> Int -> String -> GenState (Maybe LocalVar)
-getLocalVar funcName lev var = do
-        fstate <- getFuncState funcName
-        M.lookup var <$> getScope lev fstate
-
-
--- parameters
-
-getParamPos :: String -> String -> GenState (Maybe Int)
-getParamPos _ "global" = pure Nothing
-getParamPos paramName funcName =
-        extract paramNum
-        . M.lookup paramName
-        . parameters <$> getFuncState funcName
-
-
-addParam :: String -> Type -> FuncState -> FuncState
-addParam name typ fstate =
-        let parVar  = GenStateLocal.mkParVar (paramCount fstate) typ
-            fstate' = fstate { paramCount = succ . paramCount $ fstate }
-            in
-        fstate' { parameters = M.insert name parVar . parameters $ fstate' }
-
-
-extract :: (b -> a) -> Maybe b -> Maybe a
-extract f (Just pv) = Just . f $ pv
-extract _ Nothing   = Nothing
-
-
-paramData :: ParamVar -> (Int, Type)
-paramData pv = (paramNum pv, paramType pv)
-
---
+++ src/SymTabLocalVars.hs
@@ -0,0 +1,192 @@
+
+module SymTabLocalVars
+        (checkVariable,
+         variableOffset,
+         variableType,
+         addVariable,
+         addParameter,
+         parameterPosition,
+         parameterType,
+         parameterDeclared,
+         getBreak,
+         setBreak,
+         getContinue,
+         setContinue,
+         allTypes
+        ) where
+
+
+import           Data.Function     (on)
+import           Data.List         (sortBy)
+import qualified Data.Map          as M
+
+import qualified FrameStack        (currentFunc)
+import           GenState          (GenState)
+import           GenStateLocal     (FuncState (paramCount, parameters, scopes),
+                                    LocalVar (..), ParamVar (..))
+import qualified GenStateLocal     (mkLocVar, mkParVar)
+import           SymTabLocalOffset (currentOffset, incrementOffsetByN)
+import           SymTabLocalScope  (getScope, scopeDepth)
+import           SymTabLocalShared (getFuncState, setFuncState)
+import           Type              (Type (Label))
+
+
+-- | Get the break label number for current scope
+getBreak :: GenState (Maybe Int)
+getBreak = getAttribute locOffset "@Break"
+
+
+-- | Get the continue label number for current scope
+getContinue :: GenState (Maybe Int)
+getContinue = getAttribute locOffset "@Continue"
+
+
+-- | Set the break label number for current scope
+setBreak :: Int -> GenState ()
+setBreak labelNo = store "@Break" labelNo Label
+
+
+-- | Set the continue label number for current scope
+setContinue :: Int -> GenState ()
+setContinue labelNo = store "@Continue" labelNo Label
+
+
+-- | Check if variable name is in use in current scope
+checkVariable :: String -> GenState Bool
+checkVariable varName = do
+        fName  <- FrameStack.currentFunc
+        scope  <- scopeDepth fName
+        locVar <- getLocalVar fName scope varName
+        case locVar of
+             Just _  -> pure True
+             Nothing -> pure False
+
+
+-- | Get the offset from base pointer of variable
+variableOffset :: String -> GenState (Maybe Int)
+variableOffset name = getAttribute locOffset name
+
+
+-- | Get the type of variable
+variableType :: String -> GenState (Maybe Type)
+variableType name = getAttribute locType name
+
+
+-- | Store new variable, returning offset from base pointer
+addVariable :: String -> Type -> GenState Int
+addVariable varName typ = do
+        currOff <- currentOffset
+        store varName currOff typ
+        incrementOffsetByN 1
+        pure currOff
+
+
+-- | Add a new parameter to the state of a function
+addParameter :: String -> Type -> GenState ()
+addParameter paramName typ = do
+        currFuncName <- FrameStack.currentFunc
+        funcState    <- getFuncState currFuncName
+        let funcState' = addParam paramName typ funcState
+        setFuncState currFuncName funcState'
+
+
+-- | Retrieve the position of function parameter
+parameterPosition :: String -> GenState (Maybe Int)
+parameterPosition paramName = do
+        funcName <- FrameStack.currentFunc
+        getParamPos paramName funcName
+
+
+-- | Retrieve the type of function parameter
+parameterType :: String -> GenState (Maybe Type)
+parameterType paramName = do
+        currFuncName <- FrameStack.currentFunc
+        extract paramType
+            . M.lookup paramName
+            . parameters <$> getFuncState currFuncName
+
+
+-- | Retrieve list of all the type of function parameters
+allTypes :: String -> GenState [Type]
+allTypes funcName = do
+        paramList <- M.elems . parameters <$> getFuncState funcName
+        pure $ snd <$> sortBy (compare `on` fst) (paramData <$> paramList)
+
+
+-- | Check a parameter exits for function
+parameterDeclared :: String -> GenState Bool
+parameterDeclared paramName = do
+        pos <- parameterPosition paramName
+        case pos of
+             Just _  -> pure True
+             Nothing -> pure False
+
+
+-- store and lookup
+
+getAttribute :: (LocalVar -> a) -> String -> GenState (Maybe a)
+getAttribute f varName = do
+        funcName <- FrameStack.currentFunc
+        getAttr f varName funcName
+
+
+getAttr :: (LocalVar -> a) -> String -> String -> GenState (Maybe a)
+getAttr _ _ "global" = pure Nothing
+getAttr f varName funcName = do
+        scopeLevel <- scopeDepth funcName
+        extract f <$> find funcName scopeLevel varName
+
+
+find :: String -> Int -> String -> GenState (Maybe LocalVar)
+find _ (-1) _ = pure Nothing
+find funcName scope name = do
+        locVar <- getLocalVar funcName scope name
+        case locVar of
+             Nothing -> find funcName (pred scope) name
+             Just lv -> pure (Just lv)
+
+
+store :: String -> Int -> Type -> GenState ()
+store name value typ = do
+        funcName <- FrameStack.currentFunc
+        fstate   <- getFuncState funcName
+        level    <- scopeDepth funcName
+        scope    <- getScope level fstate
+        let locVar  = GenStateLocal.mkLocVar value typ
+            scope'  = M.insert name locVar scope
+            fstate' = fstate { scopes = M.insert level scope' $ scopes fstate }
+        setFuncState funcName fstate'
+
+
+getLocalVar :: String -> Int -> String -> GenState (Maybe LocalVar)
+getLocalVar funcName lev var = do
+        fstate <- getFuncState funcName
+        M.lookup var <$> getScope lev fstate
+
+
+-- parameters
+
+getParamPos :: String -> String -> GenState (Maybe Int)
+getParamPos _ "global" = pure Nothing
+getParamPos paramName funcName =
+        extract paramNum
+        . M.lookup paramName
+        . parameters <$> getFuncState funcName
+
+
+addParam :: String -> Type -> FuncState -> FuncState
+addParam name typ fstate =
+        let parVar  = GenStateLocal.mkParVar (paramCount fstate) typ
+            fstate' = fstate { paramCount = succ . paramCount $ fstate }
+            in
+        fstate' { parameters = M.insert name parVar . parameters $ fstate' }
+
+
+extract :: (b -> a) -> Maybe b -> Maybe a
+extract f (Just pv) = Just . f $ pv
+extract _ Nothing   = Nothing
+
+
+paramData :: ParamVar -> (Int, Type)
+paramData pv = (paramNum pv, paramType pv)
+
