9947cd6e138707eb57c907f90f181e3cfb968c28
James Thomas <codeactor@protonmail.com>
2020-02-20 20:35:41 +0000

    Add blank line

--- src/LexTab.hs
+++ src/LexTab.hs
@@ -14,6 +14,7 @@ import Tokens (Token)
 data LexTab = LexTab { datList :: [LexDat]
                      , lineNum :: Int }
 
+
 -- | LexTab state constructor
 mkLexTab :: LexTab
 mkLexTab = LexTab [] 1
