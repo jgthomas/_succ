718a40d0219d7092b140ab9b7b91b59ddc88cc3a
James Thomas <codeactor@protonmail.com>
2020-05-03 13:15:44 +0100

    Move variable type creation funcitons to where used

--- src/GenTokens.hs
+++ src/GenTokens.hs
@@ -16,18 +16,3 @@ data VarType = LocalVar Int Int
              | ParamVar Int Int
              | GlobalVar String Int
              deriving (Eq)
-
-
-mkVarLocal :: Maybe Int -> VarLookup
-mkVarLocal (Just n) = VarType (LocalVar n 0)
-mkVarLocal Nothing  = NotFound
-
-
-mkVarParam :: Maybe Int -> VarLookup
-mkVarParam (Just n) = VarType (ParamVar n 0)
-mkVarParam Nothing  = NotFound
-
-
-mkVarGlobal :: Maybe String -> VarLookup
-mkVarGlobal (Just s) = VarType (GlobalVar s 0)
-mkVarGlobal Nothing  = NotFound
--- src/State/SymTab.hs
+++ src/State/SymTab.hs
@@ -16,23 +16,37 @@ module State.SymTab
         ) where
 
 
-import           GenState          (GenState, labelNum)
-import           GenTokens         (VarLookup (..), VarType (..))
-import qualified GenTokens         (mkVarGlobal, mkVarLocal, mkVarParam)
-import           State.FrameStack  (currentFunc, getScope)
-import           State.FuncState
-import           State.GlobalScope
-import           State.SymbolTable (SymTab, memOffset)
+import GenState          (GenState, labelNum)
+import GenTokens         (VarLookup (..), VarType (..))
+import State.FrameStack  (currentFunc, getScope)
+import State.FuncState
+import State.GlobalScope
+import State.SymbolTable (SymTab, memOffset)
 
 
 -- | Build variable data type from retrieved data
 getVariable :: String -> GenState VarLookup
 getVariable name = do
-        localVar  <- GenTokens.mkVarLocal <$> State.FuncState.variableOffset name
-        paramVar  <- GenTokens.mkVarParam <$> State.FuncState.parameterPosition name
-        globalVar <- GenTokens.mkVarGlobal <$> State.GlobalScope.globalLabel name
+        localVar  <- mkVarLocal <$> State.FuncState.variableOffset name
+        paramVar  <- mkVarParam <$> State.FuncState.parameterPosition name
+        globalVar <- mkVarGlobal <$> State.GlobalScope.globalLabel name
         case (localVar, paramVar, globalVar) of
              (var@(VarType LocalVar{}), _, _)  -> pure var
              (_, var@(VarType ParamVar{}), _)  -> pure var
              (_, _, var@(VarType GlobalVar{})) -> pure var
              (_, _, _)                         -> pure NotFound
+
+
+mkVarLocal :: Maybe Int -> VarLookup
+mkVarLocal (Just n) = VarType (LocalVar n 0)
+mkVarLocal Nothing  = NotFound
+
+
+mkVarParam :: Maybe Int -> VarLookup
+mkVarParam (Just n) = VarType (ParamVar n 0)
+mkVarParam Nothing  = NotFound
+
+
+mkVarGlobal :: Maybe String -> VarLookup
+mkVarGlobal (Just s) = VarType (GlobalVar s 0)
+mkVarGlobal Nothing  = NotFound
