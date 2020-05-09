12383bb410ccf9e6c81b33be80237f46b5d7535d
James Thomas <codeactor@protonmail.com>
2020-05-09 12:03:27 +0100

    Add space

--- test/ParserTest/ParserExpressionSpec.hs
+++ test/ParserTest/ParserExpressionSpec.hs
@@ -14,6 +14,7 @@ import Types.Tokens
 parserExpressionTest :: IO ()
 parserExpressionTest = hspec $ do
         describe "Build abstract syntax trees for expressions" $ do
+
                 it "Should build a variable syntax tree" $
                   (extractExpressionTree [Ident "a", SemiColon])
                   `shouldBe`
