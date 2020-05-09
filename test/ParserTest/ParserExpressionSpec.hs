4b95cff9485cf708dd2150ab71ffd5edb9afa422
James Thomas <codeactor@protonmail.com>
2020-05-09 11:13:03 +0100

    Almost working unit tests

--- test/ParserTest/ParserExpressionSpec.hs
+++ test/ParserTest/ParserExpressionSpec.hs
@@ -4,10 +4,43 @@ module ParserTest.ParserExpressionSpec (parserExpressionTest) where
 
 import Test.Hspec
 
+import Parser.ParserExpression
+import Parser.ParState         as ParState
+import TestUtility             (makeLexData, makeNodeDat)
+import Types.AST
+import Types.Error
+import Types.LexDat
+import Types.Tokens
+
+
+getParsed :: Either CompilerError Tree -> Maybe Tree
+getParsed (Right tree) = Just tree
+getParsed (Left err)   = error $ show err
+
+
+extractParsed :: [LexDat] -> Either CompilerError Tree
+extractParsed lexData = runParState runTheParse lexData startState
+
+
+runTheParse :: [LexDat] -> ParserState Tree
+runTheParse [] = ProgramNode . reverse <$> getState
+runTheParse lexData = do
+        items            <- getState
+        (item, lexData') <- parseExpression lexData
+        putState $ ProgramNode (item:items)
+        runTheParse lexData'
+
+
+
+lexDataSample :: [LexDat]
+lexDataSample = makeLexData [Ident "a"]
+
 
 parserExpressionTest :: IO ()
 parserExpressionTest = hspec $ do
         describe "Dummy test" $ do
                 it "Should print dummy data" $
-                 True `shouldBe` True
+                  (getParsed . extractParsed $ lexDataSample)
+                  `shouldBe`
+                  Just (ProgramNode [VarNode "a" makeNodeDat])
 
