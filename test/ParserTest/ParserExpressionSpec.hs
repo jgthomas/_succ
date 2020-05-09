90f1c9c5f3d4962cc9e5fbe9fd2549bc0fa2b6b7
James Thomas <codeactor@protonmail.com>
2020-05-09 11:28:37 +0100

    Move all helper code to the utility

--- test/ParserTest/ParserExpressionSpec.hs
+++ test/ParserTest/ParserExpressionSpec.hs
@@ -4,30 +4,10 @@ module ParserTest.ParserExpressionSpec (parserExpressionTest) where
 
 import Test.Hspec
 
-import Parser.ParserExpression
-import Parser.ParState         as ParState
 import ParserTest.TestUtility
-import TestUtility             (makeNodeDat)
+import TestUtility            (makeNodeDat)
 import Types.AST
-import Types.Error
-import Types.LexDat
 import Types.Tokens
-
-
-getParsed :: Either CompilerError Tree -> Tree
-getParsed (Right tree) = tree
-getParsed (Left err)   = error $ show err
-
-
-extractParsed :: [LexDat] -> Either CompilerError Tree
-extractParsed lexData = runParState runTheParse lexData startState
-
-
-runTheParse :: [LexDat] -> ParserState Tree
-runTheParse lexData = do
-        (item, _) <- parseExpression lexData
-        putState $ ProgramNode [item]
-        ProgramNode .reverse <$> getState
 
 
 parserExpressionTest :: IO ()
--- test/ParserTest/TestUtility.hs
+++ test/ParserTest/TestUtility.hs
@@ -2,10 +2,30 @@
 module ParserTest.TestUtility where
 
 
-import TestUtility  (makeLexDat)
+import Parser.ParserExpression
+import Parser.ParState         as ParState
+import TestUtility             (makeLexDat)
+import Types.AST
+import Types.Error
 import Types.LexDat
 import Types.Tokens
 
 
 makeLexData :: [Token] -> [LexDat]
 makeLexData toks = map makeLexDat toks
+
+
+getParsed :: Either CompilerError Tree -> Tree
+getParsed (Right tree) = tree
+getParsed (Left err)   = error $ show err
+
+
+extractParsed :: [LexDat] -> Either CompilerError Tree
+extractParsed lexData = runParState runTheParse lexData startState
+
+
+runTheParse :: [LexDat] -> ParserState Tree
+runTheParse lexData = do
+        (item, _) <- parseExpression lexData
+        putState $ ProgramNode [item]
+        ProgramNode .reverse <$> getState
