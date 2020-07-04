63bb3e6dacb019faddc217dc9302885244b4b10e
James Thomas <codeactor@protonmail.com>
2020-07-04 14:36:58 +0100

    Add tests for parsing functions

--- test/ParserTest/ParserFunctionSpec.hs
+++ test/ParserTest/ParserFunctionSpec.hs
@@ -15,7 +15,39 @@ parserFunctionTest :: IO ()
 parserFunctionTest = hspec $ do
         describe "Build abstract syntax trees for functions" $ do
 
-                it "Should build a simple function tree" $
+                it "Should build a function declaration tree" $
+                  (extractFunctionTree [Keyword Int,
+                                        Ident "dog",
+                                        OpenBracket OpenParen,
+                                        CloseBracket CloseParen,
+                                        SemiColon
+                                       ])
+                  `shouldBe`
+                  ProgramNode [FunctionNode
+                               IntVar
+                               "dog"
+                               []
+                               Nothing
+                               mockNodeDat
+                              ]
+
+                it "Should build a simple function definition tree" $
+                  (extractFunctionTree [Keyword Int,
+                                        Ident "main",
+                                        OpenBracket OpenParen,
+                                        CloseBracket CloseParen,
+                                        OpenBracket OpenBrace,
+                                        CloseBracket CloseBrace
+                                       ])
+                  `shouldBe`
+                  ProgramNode [FunctionNode
+                               IntVar
+                               "main"
+                               []
+                               (Just [])
+                               mockNodeDat]
+
+                it "Should build a function definition with arguments tree" $
                   (extractFunctionTree [Keyword Int,
                                         Ident "main",
                                         OpenBracket OpenParen,
