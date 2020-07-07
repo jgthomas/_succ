68d42bf40326b75b344c6acfc2a7f80798b38f15
James Thomas <codeactor@protonmail.com>
2020-07-07 18:10:07 +0100

    Clone using ssh

--- func_test.sh
+++ func_test.sh
@@ -5,11 +5,11 @@ TESTS=$@
 SUCC_BIN="succ-exe"
 SUCC_BIN_PATH=$(stack exec which $SUCC_BIN)
 
-TEST_REPO="https://github.com/jgthomas/succ-functional-tests"
-TEST_DIR=$(basename $TEST_REPO)
+TEST_REPO="git@github.com:jgthomas/succ-functional-tests.git"
+TEST_DIR=$(basename ${TEST_REPO%.*})
 TEST_DIR_PATH="../$TEST_DIR"
 
-TEST_SCRIPT="./test_compiler.sh"
+TEST_SCRIPT="test_compiler.sh"
 
 ALL_TEST_CASES="literals \
                 unary \
@@ -43,4 +43,4 @@ fi
 
 
 # enter subshell in test directory and run tests
-(cd $TEST_DIR_PATH && $TEST_SCRIPT $SUCC_BIN_PATH $TEST_CASES)
+(cd $TEST_DIR_PATH && ./$TEST_SCRIPT $SUCC_BIN_PATH $TEST_CASES)
