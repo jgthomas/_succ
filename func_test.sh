7b283628e45708d621197f11f199ea323a9be072
James Thomas <codeactor@protonmail.com>
2020-07-08 18:42:03 +0100

    Default of all tests run now set in functional tests script

--- func_test.sh
+++ func_test.sh
@@ -11,36 +11,10 @@ TEST_DIR_PATH="../$TEST_DIR"
 
 TEST_SCRIPT="test_compiler.sh"
 
-ALL_TEST_CASES="literals \
-                unary \
-                binary_I \
-                binary_II \
-                locals \
-                conditionals \
-                compound \
-                loops \
-                functions \
-                globals \
-                ops \
-                pointers \
-                types \
-                bitwise \
-                array"
 
-
-# clone tests if they don't exist locally
 if [[ ! -d $TEST_DIR_PATH ]]; then
         git clone $TEST_REPO $TEST_DIR_PATH
 fi
 
 
-# set the tests to run
-if [[ ! -z $TESTS ]]; then
-        TEST_CASES=$TESTS
-else
-        TEST_CASES=$ALL_TEST_CASES
-fi
-
-
-# enter subshell in test directory and run tests
-(cd $TEST_DIR_PATH && ./$TEST_SCRIPT $SUCC_BIN_PATH $TEST_CASES)
+(cd $TEST_DIR_PATH && ./$TEST_SCRIPT $SUCC_BIN_PATH $TESTS)
