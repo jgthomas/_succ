9f016d335417e082ad9df06c31ad91bfcb4a9702
James Thomas <codeactor@protonmail.com>
2020-08-19 22:32:01 +0100

    Fix binary logical operator function

--- src/Compute/ComputeExpression.hs
+++ src/Compute/ComputeExpression.hs
@@ -25,8 +25,8 @@ binaryFunction GreaterThan          = \x y -> if x > y then 1 else 0
 binaryFunction LessThan             = \x y -> if x < y then 1 else 0
 binaryFunction GThanOrEqu           = \x y -> if x >= y then 1 else 0
 binaryFunction LThanOrEqu           = \x y -> if x <= y then 1 else 0
-binaryFunction LogicalOR            = \x y -> if x > 0 || y > 0 then 1 else 0
-binaryFunction LogicalAND           = \x y -> if x > 0 && y > 0 then 1 else 0
+binaryFunction LogicalOR            = \x y -> if x /= 0 || y /= 0 then 1 else 0
+binaryFunction LogicalAND           = \x y -> if x /= 0 && y /= 0 then 1 else 0
 binaryFunction BitwiseXOR           = xor
 binaryFunction BitwiseAND           = (.&.)
 binaryFunction BitwiseOR            = (.|.)
