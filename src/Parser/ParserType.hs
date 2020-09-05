28fbf0837ecabdb8a4d75d6b5cbb4961e49da8e4
James Thomas <codeactor@protonmail.com>
2020-09-05 16:29:53 +0100

    Neaten up parsers

--- src/Parser/ParserType.hs
+++ src/Parser/ParserType.hs
@@ -10,7 +10,6 @@ module Parser.ParserType (parseType) where
 import Parser.ParState (ParserState, throwError)
 import Types.Error     (CompilerError (ParserError, SyntaxError),
                         ParserError (..), SyntaxError (..))
---import Types.LexDat    (LexDat (..))
 import Types.Tokens
 import Types.Type      (Type (..))
 
@@ -18,8 +17,8 @@ import Types.Type      (Type (..))
 -- | Determines the type of a function or variable
 parseType :: [Token] -> ParserState Type
 parseType (Keyword Int _:rest) = parseIntType rest
-parseType (a:_)   = throwError $ SyntaxError (BadType a)
-parseType tokens = throwError $ ParserError (LexDataError tokens)
+parseType (token:_)            = throwError $ SyntaxError (BadType token)
+parseType tokens               = throwError $ ParserError (LexDataError tokens)
 
 
 parseIntType :: [Token] -> ParserState Type
@@ -31,11 +30,11 @@ parseIntType tokens
 
 isIntArray :: [Token] -> Bool
 isIntArray (_:OpenBracket OpenSqBracket _:
-              CloseBracket CloseSqBracket _:_) = True
+            CloseBracket CloseSqBracket _:_) = True
 isIntArray (_:OpenBracket OpenSqBracket _:
-              ConstInt _ _:
-              CloseBracket CloseSqBracket _:_) = True
-isIntArray _                                   = False
+            ConstInt _ _:
+            CloseBracket CloseSqBracket _:_) = True
+isIntArray _                                 = False
 
 
 isIntPointer :: [Token] -> Bool
