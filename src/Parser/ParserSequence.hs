6844dbded57fac63a99a4d73c44948f307f379da
James Thomas <codeactor@protonmail.com>
2020-09-05 16:35:08 +0100

    Neaten up parsers

--- src/Parser/ParserSequence.hs
+++ src/Parser/ParserSequence.hs
@@ -12,7 +12,6 @@ import Parser.TokConsume (consumeTok)
 import Types.AST         (Tree)
 import Types.Error       (CompilerError (ParserError, SyntaxError),
                           ParserError (..), SyntaxError (..))
---import Types.LexDat      (LexDat (..))
 import Types.Tokens
 
 
@@ -21,13 +20,20 @@ parseBracketedSeq :: [Tree]
                   -> [Token]
                   -> ([Tree] -> [Token] -> ParserState ([Tree], [Token]))
                   -> ParserState ([Tree], [Token])
+
 parseBracketedSeq _ [] _ = throwError $ ParserError (LexDataError [])
-parseBracketedSeq xs tokens@(OpenBracket _ _:CloseBracket _ _:_) _ = do
+
+parseBracketedSeq trees tokens@(OpenBracket _ _:CloseBracket _ _:_) _ = do
                                       tokens' <- consumeTok tokens
-                                      pure (reverse xs, tokens')
-parseBracketedSeq xs tokens@(CloseBracket _ _:_) _ = pure (reverse xs, tokens)
-parseBracketedSeq _ (d@(Comma _):CloseBracket _ _:_) _ =
-        throwError $ SyntaxError (UnexpectedLexDat d)
-parseBracketedSeq xs (OpenBracket _ _:rest) f = f xs rest
-parseBracketedSeq xs (Comma _:rest) f         = f xs rest
-parseBracketedSeq _ (a:_) _ = throwError $ SyntaxError (UnexpectedLexDat a)
+                                      pure (reverse trees, tokens')
+
+parseBracketedSeq trees tokens@(CloseBracket _ _:_) _ = pure (reverse trees, tokens)
+
+parseBracketedSeq _ (token@(Comma _):CloseBracket _ _:_) _ =
+        throwError $ SyntaxError (UnexpectedLexDat token)
+
+parseBracketedSeq trees (OpenBracket _ _:rest) f = f trees rest
+
+parseBracketedSeq trees (Comma _:rest) f         = f trees rest
+
+parseBracketedSeq _ (token:_) _ = throwError $ SyntaxError (UnexpectedLexDat token)
