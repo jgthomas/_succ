8a5ec448079d386e3ceb0b1be31bf27341e2a05a
James Thomas <codeactor@protonmail.com>
2020-09-12 15:52:38 +0100

    Add test module for function calls

--- test/ConverterTest/ConverterTestSpec.hs
+++ test/ConverterTest/ConverterTestSpec.hs
@@ -6,6 +6,7 @@ import ConverterTest.ConverterArraySpec       (converterArrayTest)
 import ConverterTest.ConverterDeclarationSpec (converterDeclarationTest)
 import ConverterTest.ConverterErrorSpec       (converterErrorTest)
 import ConverterTest.ConverterExpressionSpec  (converterExpressionTest)
+import ConverterTest.ConverterFuncCallSpec    (converterFuncCallTest)
 import ConverterTest.ConverterPointerSpec     (converterPointerTest)
 import ConverterTest.ConverterStatementSpec   (converterStatementTest)
 
@@ -15,6 +16,7 @@ converterTest = do
         converterDeclarationTest
         converterExpressionTest
         converterStatementTest
+        converterFuncCallTest
         converterPointerTest
         converterArrayTest
         converterErrorTest
