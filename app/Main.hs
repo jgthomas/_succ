cb87eb6513e589e5ec4dc53c1b0afda77a332298
James Thomas <codeactor@protonmail.com>
2020-09-13 15:18:03 +0100

    Better encapsulate command line argument processing

--- app/Main.hs
+++ app/Main.hs
@@ -3,24 +3,23 @@ module Main (main) where
 
 
 import           Control.DeepSeq        (deepseq)
-import           System.Console.CmdArgs
+import           System.Console.CmdArgs (cmdArgs)
 import           System.FilePath        (dropExtension)
 import           System.IO              (IOMode (ReadMode), hClose,
                                          hGetContents, openFile, writeFile)
 import           System.Process         (system)
 
-import           Options                (SuccArgs (..))
-import qualified Options                (buildOptions)
+import qualified Options                (buildCompilerOptions, options,
+                                         setFileNames)
 import qualified Succ                   (compile)
 
 
 main :: IO ()
 main = do
-        arguments <- cmdArgs options
+        arguments <- cmdArgs Options.options
 
-        let infileName     = file arguments
-            outfileName    = setOutFile infileName (asmfile arguments)
-            compileOptions = Options.buildOptions arguments
+        let (infileName, outfileName) = Options.setFileNames arguments
+            compileOptions            = Options.buildCompilerOptions arguments
 
         cFile <- openFile infileName ReadMode
         cCode <- hGetContents cFile
@@ -37,18 +36,3 @@ main = do
         _ <- system toMachineCode
         _ <- system deleteFile
         hClose cFile
-
-
-options :: SuccArgs
-options = SuccArgs {
-        debug    = False &= help "Display output of each compilation stage"
-      , optimise = False &= help "Produce optimised assembly"
-      , stage    = def &= typ "STAGE" &= help "Compilation stage to debug"
-      , asmfile  = def &= typ "FILE" &= help "Outfile name"
-      , file     = def &= argPos 0
-} &= program "succ"
-
-
-setOutFile :: FilePath -> FilePath -> FilePath
-setOutFile infile ""       = dropExtension infile ++ ".s"
-setOutFile _      filename = filename ++ ".s"
--- src/Options.hs
+++ src/Options.hs
@@ -1,9 +1,15 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 
-module Options (SuccArgs(..), SuccOptions(..), buildOptions) where
+module Options
+        (options,
+         setFileNames,
+         SuccOptions(..),
+         buildCompilerOptions
+        ) where
 
 
-import System.Console.CmdArgs (Data, Typeable)
+import System.Console.CmdArgs
+import System.FilePath        (dropExtension)
 
 import Types.SuccTokens       (Debug (..), Optimise (..))
 
@@ -17,16 +23,42 @@ data SuccArgs = SuccArgs {
 } deriving (Show, Data, Typeable)
 
 
+-- | Command line option definition
+options :: SuccArgs
+options = SuccArgs {
+        debug    = False &= help "Display output of each compilation stage"
+      , optimise = False &= help "Produce optimised assembly"
+      , stage    = def &= typ "STAGE" &= help "Compilation stage to debug"
+      , asmfile  = def &= typ "FILE" &= help "Outfile name"
+      , file     = def &= argPos 0
+} &= program "succ"
+
+
+-- | Set the input and output file names
+setFileNames :: SuccArgs -> (FilePath, FilePath)
+setFileNames arguments = (infileName, outfileName)
+        where
+                infileName  = file arguments
+                outfileName = setOutFile infileName (asmfile arguments)
+
+
+setOutFile :: FilePath -> FilePath -> FilePath
+setOutFile infile ""       = dropExtension infile ++ ".s"
+setOutFile _      filename = filename ++ ".s"
+
+
+-- | Compiler options data type
 data SuccOptions = SuccOptions {
         debugSet    :: Debug
       , optimiseSet :: Optimise
 }
 
 
-buildOptions :: SuccArgs -> SuccOptions
-buildOptions args = SuccOptions {
-        debugSet    = debugStatus (debug args) (stage args)
-      , optimiseSet = setOptimise (optimise args)
+-- | Build compiler options data container
+buildCompilerOptions :: SuccArgs -> SuccOptions
+buildCompilerOptions arguments = SuccOptions {
+        debugSet    = debugStatus (debug arguments) (stage arguments)
+      , optimiseSet = setOptimise (optimise arguments)
 }
 
 
