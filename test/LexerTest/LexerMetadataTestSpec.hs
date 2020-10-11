26ac97ceb696bedb6b1a06ac814d2178f177ddb2
James Thomas <codeactor@protonmail.com>
2020-10-11 08:02:57 +0100

    Add first metadata test

--- test/LexerTest/LexerMetadataTestSpec.hs
+++ test/LexerTest/LexerMetadataTestSpec.hs
@@ -6,12 +6,19 @@ import Data.Either
 import Test.Hspec
 
 import Lexer.Lexer
+import LexerTest.TestUtility (metaData)
 import Types.Tokens
 
 
 lexerMetadataTest :: IO ()
 lexerMetadataTest = hspec $ do
         describe "Lexing input captures correct metadata" $ do
+
+                it "Should correctly extract metadata for syntactic tokens" $
+                  (map metaData $ fromRight []
+                   (tokenize "(\n)\n"))
+                  `shouldBe`
+                  [("(", 1), (")", 2)]
 
                 it "Should record the correct line for each token" $
                   (map (line . tokenData) $
--- test/LexerTest/TestUtility.hs
+++ test/LexerTest/TestUtility.hs
@@ -1,8 +1,8 @@
 
-module LexerTest.TestUtility (dummyData) where
+module LexerTest.TestUtility (dummyData, metaData) where
 
 
-import Types.Tokens
+import Types.Tokens (LexDat (..), Token (..), dummyLexDat, tokenData)
 
 
 dummyData :: [Token] -> [Token]
@@ -10,13 +10,14 @@ dummyData tokens = map setDummy tokens
 
 
 setDummy :: Token -> Token
-setDummy (Separator SemiColon _) = Separator SemiColon dummyLexDat
-setDummy (Separator Colon _)     = Separator Colon dummyLexDat
-setDummy (Separator QuestMark _) = Separator QuestMark dummyLexDat
-setDummy (Separator Comma _)     = Separator Comma dummyLexDat
-setDummy (OpenBracket a _)       = OpenBracket a dummyLexDat
-setDummy (CloseBracket a _)      = CloseBracket a dummyLexDat
-setDummy (Ident a _)             = Ident a dummyLexDat
-setDummy (ConstInt a _)          = ConstInt a dummyLexDat
-setDummy (Keyword a _)           = Keyword a dummyLexDat
-setDummy (OpTok a _)             = OpTok a dummyLexDat
+setDummy (Separator a _)    = Separator a dummyLexDat
+setDummy (OpenBracket a _)  = OpenBracket a dummyLexDat
+setDummy (CloseBracket a _) = CloseBracket a dummyLexDat
+setDummy (Ident a _)        = Ident a dummyLexDat
+setDummy (ConstInt a _)     = ConstInt a dummyLexDat
+setDummy (Keyword a _)      = Keyword a dummyLexDat
+setDummy (OpTok a _)        = OpTok a dummyLexDat
+
+
+metaData :: Token -> (String, Int)
+metaData token = (input . tokenData $ token, line . tokenData $ token)
