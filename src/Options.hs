2751aa55c8187ac3af9573bf150cd4a198917acd
James Thomas <codeactor@protonmail.com>
2020-10-18 10:27:08 +0100

    Ormolu formatting

--- src/Options.hs
+++ src/Options.hs
@@ -1,75 +1,70 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 
 module Options
-        (options,
-         setFileNames,
-         buildCompilerOptions
-        ) where
-
+  ( options,
+    setFileNames,
+    buildCompilerOptions,
+  )
+where
 
 import System.Console.CmdArgs
-import System.FilePath        (dropExtension)
+import System.FilePath (dropExtension)
+import Types.SuccTokens (Debug (..), Optimise (..), SuccOptions (..))
 
-import Types.SuccTokens       (Debug (..), Optimise (..), SuccOptions (..))
-
-
-data SuccArgs = SuccArgs {
-        debug    :: Bool
-      , optimise :: Bool
-      , stage    :: String
-      , asmfile  :: String
-      , file     :: FilePath
-} deriving (Show, Data, Typeable)
-
+data SuccArgs
+  = SuccArgs
+      { debug :: Bool,
+        optimise :: Bool,
+        stage :: String,
+        asmfile :: String,
+        file :: FilePath
+      }
+  deriving (Show, Data, Typeable)
 
 -- | Command line option definition
 options :: SuccArgs
-options = SuccArgs {
-        debug    = False &= help "Display output of each compilation stage"
-      , optimise = False &= help "Produce optimised assembly"
-      , stage    = def &= typ "STAGE" &= help "Compilation stage to debug"
-      , asmfile  = def &= typ "FILE" &= help "Outfile name"
-      , file     = def &= argPos 0
-} &= program "succ"
-
+options =
+  SuccArgs
+    { debug = False &= help "Display output of each compilation stage",
+      optimise = False &= help "Produce optimised assembly",
+      stage = def &= typ "STAGE" &= help "Compilation stage to debug",
+      asmfile = def &= typ "FILE" &= help "Outfile name",
+      file = def &= argPos 0
+    }
+    &= program "succ"
 
 -- | Set the input and output file names
 setFileNames :: SuccArgs -> (FilePath, FilePath)
 setFileNames arguments = (infileName, outfileName)
-        where
-                infileName  = file arguments
-                outfileName = setOutFile infileName (asmfile arguments)
-
+  where
+    infileName = file arguments
+    outfileName = setOutFile infileName (asmfile arguments)
 
 setOutFile :: FilePath -> FilePath -> FilePath
-setOutFile infile ""       = dropExtension infile ++ ".s"
-setOutFile _      filename = filename ++ ".s"
-
+setOutFile infile "" = dropExtension infile ++ ".s"
+setOutFile _ filename = filename ++ ".s"
 
 -- | Build compiler options data container
 buildCompilerOptions :: SuccArgs -> SuccOptions
-buildCompilerOptions arguments = SuccOptions {
-        debugSet    = debugStatus (debug arguments) (stage arguments)
-      , optimiseSet = setOptimise (optimise arguments)
-}
-
+buildCompilerOptions arguments = SuccOptions
+  { debugSet = debugStatus (debug arguments) (stage arguments),
+    optimiseSet = setOptimise (optimise arguments)
+  }
 
 setOptimise :: Bool -> Optimise
-setOptimise True  = OptimiseOn
+setOptimise True = OptimiseOn
 setOptimise False = OptimiseOff
 
-
 debugStatus :: Bool -> String -> Debug
-debugStatus False _         = DebugOff
+debugStatus False _ = DebugOff
 debugStatus True debugStage = setDebugStatus debugStage
 
-
 setDebugStatus :: String -> Debug
-setDebugStatus "lexer"  = DebugLexer
+setDebugStatus "lexer" = DebugLexer
 setDebugStatus "parser" = DebugParser
 setDebugStatus "schema" = DebugSchema
-setDebugStatus "state"  = DebugState
-setDebugStatus "asm"    = DebugAsm
-setDebugStatus "code"   = DebugCode
-setDebugStatus "trees"  = DebugTrees
-setDebugStatus _        = DebugOn
+setDebugStatus "state" = DebugState
+setDebugStatus "asm" = DebugAsm
+setDebugStatus "code" = DebugCode
+setDebugStatus "trees" = DebugTrees
+setDebugStatus _ = DebugOn
--- src/Succ.hs
+++ src/Succ.hs
@@ -1,34 +1,34 @@
-{-|
-Module       : Succ
-Description  : Run compilation process
+-- |
+-- Module       : Succ
+-- Description  : Run compilation process
+--
+-- Controls the output of the compilation process.
+module Succ
+  ( compile,
+  )
+where
 
-Controls the output of the compilation process.
--}
-module Succ (compile) where
-
-
-import qualified Builder.Builder       as Builder (build)
-import qualified Converter.Converter   as Converter (convert)
-import qualified Debug.Debug           as Debug (debug, debugPair)
-import qualified Lexer.Lexer           as Lexer (tokenize)
-import qualified Parser.Parser         as Parser (parse)
+import qualified Builder.Builder as Builder (build)
+import qualified Converter.Converter as Converter (convert)
+import qualified Debug.Debug as Debug (debug, debugPair)
+import qualified Lexer.Lexer as Lexer (tokenize)
+import qualified Parser.Parser as Parser (parse)
 import qualified PrintError.PrintError as PrintError (handleError)
-import           Types.SuccTokens      (Stage (..), SuccOptions (..))
-
+import Types.SuccTokens (Stage (..), SuccOptions (..))
 
 -- | Run the compilation process
 compile :: String -> SuccOptions -> IO String
 compile input options = do
-        input' <- debugInput . pure $ input
-        toks   <- debugLexer . errorHandler . Lexer.tokenize $ input'
-        ast    <- debugParser . errorHandler . Parser.parse $ toks
-        schema <- fmap fst . debugSchema . errorHandler . Converter.convert $ ast
-        debugAssembly . errorHandler . Builder.build options $ schema
-        where
-                debugFlag     = debugSet options
-                debugInput    = Debug.debug debugFlag Input
-                debugLexer    = Debug.debug debugFlag Lexer
-                debugParser   = Debug.debug debugFlag Parser
-                debugSchema   = Debug.debugPair debugFlag (Schema, State)
-                debugAssembly = Debug.debug debugFlag Output
-                errorHandler  = PrintError.handleError debugFlag input
+  input' <- debugInput . pure $ input
+  toks <- debugLexer . errorHandler . Lexer.tokenize $ input'
+  ast <- debugParser . errorHandler . Parser.parse $ toks
+  schema <- fmap fst . debugSchema . errorHandler . Converter.convert $ ast
+  debugAssembly . errorHandler . Builder.build options $ schema
+  where
+    debugFlag = debugSet options
+    debugInput = Debug.debug debugFlag Input
+    debugLexer = Debug.debug debugFlag Lexer
+    debugParser = Debug.debug debugFlag Parser
+    debugSchema = Debug.debugPair debugFlag (Schema, State)
+    debugAssembly = Debug.debug debugFlag Output
+    errorHandler = PrintError.handleError debugFlag input
