32427237a4e4b4a94138487dcb5204abc36df0e0
James Thomas <codeactor@protonmail.com>
2020-11-27 13:51:54 +0000

    Split top level assemble function from implementations

--- src/Assembler/Assembler.hs
+++ src/Assembler/Assembler.hs
@@ -7,8 +7,12 @@ import System.Exit (ExitCode)
 import System.FilePath (dropExtension)
 import System.Process (system)
 
+-- | Convert assembly into machine code
 assemble :: FilePath -> IO ExitCode
-assemble asmFileName = system $ assembler ++ opts ++ asmFileName ++ binaryOut
+assemble asmFileName = gccAssemble asmFileName
+
+gccAssemble :: FilePath -> IO ExitCode
+gccAssemble asmFileName = system $ assembler ++ opts ++ asmFileName ++ binaryOut
   where
     assembler = "gcc "
     opts = "-g "
